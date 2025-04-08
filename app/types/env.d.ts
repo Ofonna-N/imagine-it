1f67dd4c0a4f5fd9d127b44f0d0a90126979dae1
Ofonna Nweze <nwezeofonna@gmail.com>
2025-04-08 01:08:16 -0500

    Remove unused VITE_SUPABASE_ANON_KEY from ProcessEnv interface in env.d.ts

--- app/types/env.d.ts
+++ app/types/env.d.ts
@@ -5,7 +5,6 @@ declare global {
       PRINTFUL_BASE_URL: string;
       PRINTFUL_ACCESS_TOKEN: string;
       SUPABASE_URL: string;
-      VITE_SUPABASE_ANON_KEY: string;
       SUPABASE_SERVICE_ROLE_KEY: string;
     }
   }
