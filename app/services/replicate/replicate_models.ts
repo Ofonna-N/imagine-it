a9110fc57e8b9afc411e65d12fd1528b594a3224
Ofonna Nweze <nwezeofonna@gmail.com>
2025-04-18 21:15:35 -0500

    Add REPLICATE_MODELS for image generation with HiDream-I1 model

---
+++ app/services/replicate/replicate_models.ts
@@ -0,0 +1,13 @@
+const REPLICATE_MODELS = {
+  IMAGE_GENERATION: {
+    /* 
+    HiDream-I1 is a new open-source image generative foundation model with 17B 
+    parameters that achieves state-of-the-art image generation quality within seconds.
+    https://replicate.com/prunaai/hidream-l1-fast
+    */
+    "prunaai / hidream-l1-fast":
+      "prunaai/hidream-l1-fast:17c237d753218fed0ed477cb553902b6b75735f48c128537ab829096ef3d3645",
+  },
+};
+
+export default REPLICATE_MODELS;
